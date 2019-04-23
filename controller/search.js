25dfe8f6628384bdd94e6de2072142503397f618
Martin Tõnts <martin@dolmit.com>
2019-04-23 15:02:59 +0300

    changed query type

--- controller/search.js
+++ controller/search.js
@@ -47,7 +47,7 @@ function setup( apiConfig, esclient, query, should_execute ){
     // elasticsearch command
     const cmd = {
       index: apiConfig.indexName,
-      searchType: 'dfs_query_then_fetch',
+      searchType: 'query_then_fetch',
       body: renderedQuery.body
     };
 
